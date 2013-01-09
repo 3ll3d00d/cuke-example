3ae398deb09cf3bb618e3a4ba59712b8a0ad3e28
3ll3d00d <mattkhan@gmail.com>
2013-01-09 00:22:42 +0000

    initial commit of a simple gradle/cuke example

---
+++ src/test/java/ExampleStepDefs.java
@@ -0,0 +1,21 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/** @author khanmat */
+public class ExampleStepDefs {
+    @Given("^I am testing gradle and cucumber$")
+    public void I_am_testing_gradle_and_cucumber() throws Throwable {
+        // do nothing!
+    }
+
+    @When("^I run this test$")
+    public void I_run_this_test() throws Throwable {
+        // still do nothing
+    }
+
+    @Then("^good thing happen$")
+    public void good_thing_happen() throws Throwable {
+        // and keep doing nothing!
+    }
+}
---
+++ src/test/resources/example.feature
@@ -0,0 +1,6 @@
+Feature: example.feature
+
+  Scenario: A simple test
+    Given I am testing gradle and cucumber
+    When I run this test
+    Then good thing happen
